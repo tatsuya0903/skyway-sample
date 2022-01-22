ef3920d10b7d5560f365f6af11497ec6b27f53b4
tatsuya0903 <tama.and.pochi.0903@gmail.com>
2022-01-22 13:41:22 +0900

    @vue/composition-apiを利用する設定

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,4 +1,6 @@
 declare module '*.vue' {
-  import Vue from 'vue'
-  export default Vue
+  import type { defineComponent } from '@vue/composition-api'
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  const component: defineComponent<{}, {}, unknown>
+  export default component
 }
